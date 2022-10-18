a51e0b2e7d7a7ba01a553a5f107e37547d043eeb
yanochkaku <yanochkaku@gmail.com>
2022-10-18 10:58:54 +0300

    Done

---
+++ Recipes/Recipes.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Recipes.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
