f022659d52c5b9405a94ca80e913a1be05a9b096
Sofia <sofiia.avtomchuk@oa.edu.ua>
2022-10-20 11:08:10 +0200

    6 commit

---
+++ Recipes/Recipes.UI/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Recipes.Core;
+
+namespace Recipes.UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Category> AllCategories = new List<Category>
+            {
+            };
+            return View();
+        }
+        public IEnumerable<Category> AllCategories
+        {
+            get
+            {
+                return new List<Category>
+                {
+                    new Category { NameCategory = "Перші страви"},
+                    new Category { NameCategory = "Другі страви"},
+                    new Category { NameCategory = "Десерти"}
+                };
+            }
+        }
+    }
+}
